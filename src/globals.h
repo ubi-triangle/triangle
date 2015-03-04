87ccbfeeb1480575800c21e233b0d680ca76f542
Jang HyunJun <pkthads@gmail.com>
2015-03-04 09:33:30 +0900

    skeletonfile
    새로 설치하니 돌아가네요

---
+++ src/globals.h
@@ -0,0 +1,26 @@
+/****************************************************
+	Project: Triangles
+	File: globals.h
+	Description: ....
+		....
+		....
+*****************************************************/
+
+#ifndef _GLOBALS_H_
+#define _GLOBALS_H_
+
+#define FALSE 0
+#define TRUE 1
+
+typedef enum {
+	/* 삼각형 타입들...*/
+	wjd, dlemdqus
+}TriangleType;
+
+typedef struct triagles {
+	double a;
+	double b;
+	double c;
+}TRIANGLE;
+
+#endif u                                                                                                                                                                                       
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#inlcude "tri_algorithm.h"
+#include "tri_inout.h"
+
+int main()
+{
+	
+}
